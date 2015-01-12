2a2e9841e49091c347c4d44795a2eccbbfab669c
johipe <johipe@gmail.com>
2015-01-12 13:38:13 -0500

    first lesson

---
+++ WebApplication1/Scripts/game.ts
@@ -0,0 +1,17 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+
+    }
+
+    fights() {
+        console.log("He Fights")
+        }
+} 
+
+function main()
+{
+    var tom = new Player();
+    tom.fights();
+}
